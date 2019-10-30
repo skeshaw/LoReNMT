9a0901bc383055af8f9bfd0097ca222c380ff3e3
Keshaw Singh <skeshaw@outlook.com>
2019-10-30 22:50:28 +0530

    Download pretrained word embeddings

---
+++ fairseq/download_embed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# # for Basque
+# curl -o data-bin/wiki.eu.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.eu.vec
+
+# for Gujarati
+curl -o data-bin/wiki.gu.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.gu.vec
+
+# for English
+curl -o data-bin/wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.en.vec
